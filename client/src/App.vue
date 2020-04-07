00863f36ac7120bfee36bbac5258d58a6511926c
Zahir Bachnou <zahir.bachnou18@gmail.com>
2020-04-07 23:03:34 +0200

    Add to routes in the project.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
+    <h1>Bonjour l'application !</h1>
+    <p>
+      <!-- utilisez le composant router-link pour la navigation. -->
+      <!-- spécifiez le lien en le passant à la prop `to` -->
+      <!-- `<router-link>` sera rendu en tag `<a>` par défaut -->
+      <router-link to="/">Aller à l'acceuil</router-link>
+      <router-link to="/soap">Aller à Soap</router-link>
+    </p>
+    <!-- balise pour le composant router-view -->
+    <!-- le composant correspondant à la route sera rendu ici -->
+    <router-view></router-view>
+  </div>
+  <!--<div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div>-->
 </template>
 
 <script>
