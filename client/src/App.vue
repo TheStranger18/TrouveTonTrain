e37194c5c43c8063837eed6f73b69f7d2614ce20
Zahir Bachnou <zahir.bachnou@protonmail.com>
2020-04-12 14:47:14 +0200

    Applcation of the new routes in the main component.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div id="app">
-    <h1>Bonjour l'application !</h1>
-    <p>
-      <!-- utilisez le composant router-link pour la navigation. -->
-      <!-- spécifiez le lien en le passant à la prop `to` -->
-      <!-- `<router-link>` sera rendu en tag `<a>` par défaut -->
-      <router-link to="/">Aller à l'acceuil</router-link>
-      <router-link to="/soap">Aller à Soap</router-link>
-    </p>
-    <!-- balise pour le composant router-view -->
-    <!-- le composant correspondant à la route sera rendu ici -->
-    <router-view></router-view>
+    <nav>
+      <ul>
+        <li>
+          <router-link to='/'>Home</router-link>
+        </li>
+        <li>
+          <router-link to='/soap'>Soap</router-link>
+        </li>
+      </ul>
+    </nav>
+    <router-view />
   </div>
-  <!--<div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>-->
 </template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
