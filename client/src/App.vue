71d4b2e2829bceba6da71d7392857953392d33b1
Zahir Bachnou <zahir.bachnou@protonmail.com>
2020-04-13 18:05:08 +0200

    Adding the new package vue-autosuggest.

--- client/src/App.vue
+++ client/src/App.vue
@@ -34,25 +34,3 @@ export default {
   margin-top: 60px;
 }
 </style>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
