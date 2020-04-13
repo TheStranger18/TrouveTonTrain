baa26d24f08e65994335bd6c44cc06f370b4ec75
Zahir Bachnou <zahir.bachnou@protonmail.com>
2020-04-13 23:50:36 +0200

    Delete one component.

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,41 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="home">
+  
+ </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAutosuggest from 'vue-autosuggest'
+
+Vue.use(VueAutosuggest)
+Vue.use(axios)
+
+var temp = []
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'home',
+
+  created() {
+  axios.get('https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&rows=3270&sort=gare_alias_libelle_noncontraint&facet=gare_agencegc_libelle&facet=gare_regionsncf_libelle&facet=gare_ug_libelle&facet=pltf_departement_libellemin&facet=pltf_segmentdrg_libelle')
+    .then(function (response) {
+      // handle success
+      // fields.gare_alias_libelle_noncontraint
+      console.log(response.data.records)
+      var i = null
+      for (i = 0; i < 3270; i++) {
+        temp[i] = response.data.records[i].fields.gare_alias_libelle_noncontraint
+        //console.log(temp[i])
+      } 
+    })
+  },
+
+  data() {
+      return {
+        selected: '',
+        options: [{
+          data: ['Frodo', 'Samwise', 'Gandalf', 'Galadriel', 'Faramir', 'Éowyn', 'Peregrine Took', 'Boromir', 'Legolas', 'Gimli', 'Gollum', 'Beren', 'Saruman', 'Sauron', 'Théoden']
+        }],
+        filteredOptions: [],
+        inputProps: {
+          id: "autosuggest__input",
+          onInputChange: this.onInputChange,
+          placeholder: "Type 'e'"
+        },
+        limit: 10
+      };
+    },
+    methods: {
+      onSelected(option) {
+          this.selected = option.item;
+      },
+      onInputChange(text) {
+        if (text === '' || text === undefined) {
+          return;
+        }
+        
+        /* Full control over filtering. Maybe fetch from API?! Up to you!!! */
+        const filteredData = this.options[0].data.filter(item => {
+          return item.toLowerCase().indexOf(text.toLowerCase()) > -1;
+        }).slice(0, this.limit);
+        
+        this.filteredOptions = [{
+          data: filteredData
+        }];
+      }
+    }
 }
 </script>
 
@@ -55,4 +84,81 @@ li {
 a {
   color: #42b983;
 }
+
+body {
+      max-width: 800px;
+      padding: 20px;
+      margin-left: auto !important;
+      margin-right: auto !important;
+      font-family: monospace;
+    }
+    
+    #autosuggest__input {
+      outline: none;
+      position: relative;
+      display: block;
+      font-family: monospace;
+      font-size: 20px;
+      border: 1px solid #616161;
+      padding: 10px;
+      width: 100%;
+      box-sizing: border-box;
+      -webkit-box-sizing: border-box;
+      -moz-box-sizing: border-box;
+    }
+    
+    #autosuggest__input.autosuggest__input-open {
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+    
+    .autosuggest__results-container {
+      position: relative;
+      width: 100%;
+    }
+    
+    .autosuggest__results {
+      font-weight: 300;
+      margin: 0;
+      position: absolute;
+      z-index: 10000001;
+      width: 100%;
+      border: 1px solid #e0e0e0;
+      border-bottom-left-radius: 4px;
+      border-bottom-right-radius: 4px;
+      background: white;
+      padding: 0px;
+      overflow: scroll;
+      max-height: 200px;
+    }
+    
+    .autosuggest__results ul {
+      list-style: none;
+      padding-left: 0;
+      margin: 0;
+    }
+    
+    .autosuggest__results .autosuggest__results_item {
+      cursor: pointer;
+      padding: 15px;
+    }
+    
+    #autosuggest ul:nth-child(1) > .autosuggest__results_title {
+      border-top: none;
+    }
+    
+    .autosuggest__results .autosuggest__results_title {
+      color: gray;
+      font-size: 11px;
+      margin-left: 0;
+      padding: 15px 13px 5px;
+      border-top: 1px solid lightgray;
+    }
+    
+    .autosuggest__results .autosuggest__results_item:active,
+    .autosuggest__results .autosuggest__results_item:hover,
+    .autosuggest__results .autosuggest__results_item:focus,
+    .autosuggest__results .autosuggest__results_item.autosuggest__results_item-highlighted {
+      background-color: #ddd;
+    }
 </style>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,9 @@
 import HomePage from "./components/HomePage.vue";
-import Soap from "./components/Soap.vue";
+//import Soap from "./components/Soap.vue";
 
 const routes = [
     { path: "/", component: HomePage },
-    { path: "/soap", component: Soap }
+    //{ path: "/soap", component: Soap }
 ];
 
 export default routes;
