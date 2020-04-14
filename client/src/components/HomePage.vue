ea51e000d927fe4a47108bd06109843eb50daa84
Zahir Bachnou <zahir.bachnou@protonmail.com>
2020-04-14 18:30:19 +0200

    In progress for the auto-suggest form.

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="hello">
-
+   <div v-if="selected">
+      You have selected <code>{{selected}}</code>
+    </div>
+    <div>
+      <vue-autosuggest
+        v-model="query"
+        :suggestions="filteredOptions"
+        @focus="focusMe"
+        @click="clickHandler"
+        @input="onInputChange"
+        @selected="onSelected"
+        :get-suggestion-value="getSuggestionValue"
+        :input-props="{id:'autosuggest__input', placeholder:'Do you feel lucky, punk?'}">
+        <div v-if="query">
+          <ul>
+            <li v-for="suggestion in suggestions" :key="suggestion.name">
+              {{suggestion.name}}
+            </li>
+          </ul>
+        </div>
+      </vue-autosuggest>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import axios from 'axios'
+import VueAutosuggest from "vue-autosuggest";
+
+Vue.use(VueAutosuggest);
 
 var temp = []
 
@@ -14,9 +39,8 @@ export default {
   created() {
   axios.get('https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&rows=3270&sort=gare_alias_libelle_noncontraint&facet=gare_agencegc_libelle&facet=gare_regionsncf_libelle&facet=gare_ug_libelle&facet=pltf_departement_libellemin&facet=pltf_segmentdrg_libelle')
     .then(function (response) {
-      // handle success
       // fields.gare_alias_libelle_noncontraint
-      console.log(response.data.records)
+      //console.log(response.data.records)
       var i = null
       for (i = 0; i < 3270; i++) {
         temp[i] = response.data.records[i].fields.gare_alias_libelle_noncontraint
@@ -24,6 +48,58 @@ export default {
       } 
     })
   },
+
+  data() {
+    return {
+      query: "",
+      selected: "",
+      suggestions: [
+        
+          {name: "Hobbit"},
+           {name: "Samwise"},
+            {name: "Gandalf"},
+             {name: "Aragorn"}
+          
+        
+      ]
+    };
+  },
+  computed: {
+    filteredOptions() {
+      return [
+        { 
+          data: this.suggestions.filter(option => {
+            console.log("option="+option)
+            console.log("suggestions="+this.suggestions)
+            return option.name.toLowerCase().indexOf(this.query.toLowerCase()) > -1;
+          })
+        }
+      ];
+    }
+  },
+  methods: {
+    clickHandler(item) {
+      // event fired when clicking on the input
+      console.log(item)
+
+    },
+    onSelected(item) {
+      this.selected = item.item.name;
+    },
+    onInputChange(text) {
+      // event fired when the input changes
+      console.log(text)
+    },
+    /**
+     * This is what the <input/> value is set to when you are selecting a suggestion.
+     */
+    getSuggestionValue(suggestion) {
+      return suggestion;
+    },
+    focusMe(e) {
+      console.log(e) // FocusEvent
+    }
+  }
 }
 </script>
 
