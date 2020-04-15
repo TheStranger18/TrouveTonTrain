a53fa2058ee4c36e3b98636723778ef60fe1b7b4
Zahir Bachnou <zahir.bachnou@protonmail.com>
2020-04-15 19:58:37 +0200

    Add the service CalculDistance with restful API.

--- server/rest/app.py
+++ server/rest/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+import math
 
 app = Flask(__name__)
 api = Api(app)
 
 todos = {}
+
 
 class TodoSimple(Resource):
     def get(self, todo_id):
@@ -14,7 +16,22 @@ class TodoSimple(Resource):
         todos[todo_id] = request.form['data']
         return {todo_id: todos[todo_id]}
 
+class CalculDistance(Resource):
+    def get(self, latitudeA, longitudeA, latitudeB, longitudeB):
+
+        latitudeA=math.radians(latitudeA)
+        longitudeA=math.radians(longitudeA)
+        latitudeB=math.radians(latitudeB)
+        longitudeB=math.radians(longitudeB)
+        dlongitude=longitudeB-longitudeA
+        radian = math.acos(math.sin(latitudeA)*math.sin(latitudeB)+math.cos(latitudeA)*math.cos(latitudeB)*math.cos(dlongitude))
+        result = radian*6378137
+        result = result/1000
+        return {'distance': round(result, 2)}
+
+
 api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(CalculDistance, '/calculdistance/<float:latitudeA>/<float:longitudeA>/<float:latitudeB>/<float:longitudeB>')
 
 if __name__ == '__main__':
     app.run(debug=True)
