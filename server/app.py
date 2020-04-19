b9933c94b21754b3823afc0aedeb3f33c948b73a
Zahir Bachnou <zahir.bachnou@protonmail.com>
2020-04-19 23:53:18 +0200

    Modification of the SOAP service.

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ from spyne.protocol.soap import Soap11
 from spyne.model.primitive import Unicode, Integer
 from spyne.model.complex import Iterable
 from flask_cors import CORS
+import math
 
 app = Flask(__name__)
 spyne = Spyne(app)
@@ -15,9 +16,15 @@ class SomeSoapService(spyne.Service):
     __out_protocol__ = Soap11()
 
     @spyne.srpc(Unicode, Integer, _returns=Iterable(Unicode))
-    def echo(str, cnt):
-        for i in range(cnt):
-            yield str
+    def distance(self, longitudeA, latitudeA, longitudeB, latitudeB):
+        longitudeA=math.radians(longitudeA)
+        latitudeA=math.radians(latitudeA)
+        longitudeB=math.radians(longitudeB)
+        latitudeB=math.radians(latitudeB)
+        dlongitude=longitudeB-longitudeA
+        radian = math.acos(math.sin(latitudeA)*math.sin(latitudeB)+math.cos(latitudeA)*math.cos(latitudeB)*math.cos(dlongitude))
+        result = radian*6378137
+        return result
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
